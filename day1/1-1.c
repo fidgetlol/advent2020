f1c5fe0da21d4668263a21ab1161a54075bdb544
fidgetlol <matthewgure@gmail.com>
2020-12-03 13:25:59 -0500

    stupid ordered sets

--- day1/1-1.c
+++ day1/1-1.c
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <set>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
 
 int main() {
-  set<long> pool;
+  unordered_set<long> pool;
   vector<long> nums;
   long n, len = 0;
 
@@ -16,8 +16,10 @@ int main() {
   }
 
   for(long i=0; i < len; i++) {
-    if (pool.count(2020-nums[i])!=0)
+    if (pool.count(2020-nums[i])!=0) {
       cout << (2020-nums[i]) * nums[i] << endl;
+      break;
+    }
   }
 
   return 0;
